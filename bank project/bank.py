b3d7c9d4d1bb894ef7ef648ea14668899214f565
piyushcodevns <168630881+piyushcodevns@users.noreply.github.com>
2025-05-03 15:27:51 +0530

    update

--- bank project/bank.py
+++ bank project/bank.py
@@ -1,16 +1,36 @@
 from person import Person
+
+
 class BankAccount:
     def __init__(self, customer, bank_name, accountno, balance=0):
-        self.customer = customer 
-        self.bank_name = bank_name
+        self.name = customer
+        self.bank_name = "PNB Piyush National Bank"
         self.accountno = accountno
         self.balance = balance
 
+    def deposit(self):
+        amount = float(input("Enter deposit amount: "))
+        if amount <= 0:
+            print("Invalid amount. please enter 0positive number")
+        else:
+            self.balance += amount
+            print("Updated Balance:", self.balance)
+
+    def withdraw(self):
+        amount = float(input("Enter withdrawal amount: "))
+        if amount <= 0:
+            print("Invalid amount. Please enter a positive number.")
+        elif self.balance >= amount:
+            self.balance -= amount
+            print("Updated Balance:", self.balance)
+        else:
+            print("Insufficient balance")
+
     def detail(self):
-        print(f"Account Holder: {self.customer}")
+        print(f"Account Holder: {self.name}")
         print(f"Bank Name: {self.bank_name}")
         print(f"Account Number: {self.accountno}")
         print(f"Balance: {self.balance}")
 
     def __str__(self):
-        return f"Account Holder: {self.customer}, Bank Name: {self.bank_name}, Account Number: {self.accountno}, Balance: {self.balance}"
+        return f"Account Holder: {self.name}, Bank Name: {self.bank_name}, Account Number: {self.accountno}, Balance: {self.balance}"
