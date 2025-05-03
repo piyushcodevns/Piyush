620b393dd6de414f708ab3cf31e7fcd43eeff11b
piyushcodevns <168630881+piyushcodevns@users.noreply.github.com>
2025-05-03 16:09:21 +0530

    update

--- bank project/currency.py
+++ bank project/currency.py
@@ -18,23 +18,3 @@ class Currency:
             raise ValueError("Insufficient balance")
         remaining = total_self - total_other
         return Currency(remaining // 100, remaining % 100)
-
-
-class BankAccount:
-    def __init__(self, customer, bank_name, accountno, balance=None):
-        self.customer = customer
-        self.bank_name = bank_name
-        self.accountno = accountno
-        self.balance = balance or Currency(0, 0)
-
-    def deposit(self, rupees, paisa):
-        self.balance = self.balance.add(Currency(rupees, paisa))
-
-    def withdraw(self, rupees, paisa):
-        self.balance = self.balance.subtract(Currency(rupees, paisa))
-
-    def detail(self):
-        return f"Account Holder: {self.customer}, Bank Name: {self.bank_name}, Account Number: {self.accountno}, Balance: {self.balance}"
-
-    def __str__(self):
-        return self.detail()
