cf3f8b38eb9348d9665e9eada5d05b98816f94c3
piyushcodevns <168630881+piyushcodevns@users.noreply.github.com>
2025-05-05 13:02:58 +0530

    p

--- bank project/testfile.py
+++ bank project/testfile.py
@@ -8,6 +8,6 @@ c2 = Currency(5, 75)
 p = Person("Piyush", 18, "Varanasi")
 b = BankAccount(p, "PNB Piyush National Bank", 12345, 1000) 
 digit= Digit(56474)
-
+print(digit)
 print(c1,c2)
 print(c1+ c2)
